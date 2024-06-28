37d53321f9ce791d8029c9540c0e5eeab36f4970
DecafDev <40307803+decaf-dev@users.noreply.github.com>
2024-06-27 22:17:20 -0600

    Make command always available (#6)
    * docs: add note about being in editing mode
    
    * fix: always keep command available
    
    * refactor: move into separate function
    
    * chore: bump version
    
    * docs: remove step about going into editing mode

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Editor, MarkdownView, Notice, Plugin, normalizePath } from "obsidian";
+import { MarkdownView, Notice, Plugin, TFile, normalizePath } from "obsidian";
 import { escapeInvalidFileNameChars, findFrontmatterEndIndex, trimForFileName } from "./utils";
 import NoteSplitterSettingsTab from "./obsidian/note-splitter-settings-tab";
 
@@ -27,101 +27,114 @@ export default class NoteSplitterPlugin extends Plugin {
 		this.addCommand({
 			id: "split-by-delimiter",
 			name: "Split by delimiter",
-			editorCallback: async (_editor: Editor, view: MarkdownView) => {
+			callback: async () => {
+				const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+				if (view === null) {
+					new Notice("Please open a markdown note.");
+					return;
+				}
+
 				const file = view.file;
-				if (file === null) return;
-
-				//Obsidian will store `\n`` as `\\n` in the settings
-				const delimiter = this.settings.delimiter.replace(/\\n/g, "\n");
-
-				if (delimiter === "") {
-					new Notice("No delimiter set. Please set a delimiter in the settings.");
+				if (file === null) {
+					new Notice("No file found for this note.");
 					return;
 				}
 
-				const fileData = await this.app.vault.cachedRead(file);
-				const frontmatterEndIndex = findFrontmatterEndIndex(fileData);
-
-				let dataWithoutFrontmatter = fileData;
-				//Ignore frontmatter
-				if (frontmatterEndIndex !== -1) {
-					dataWithoutFrontmatter = dataWithoutFrontmatter.slice(frontmatterEndIndex + 1);
-				}
-				if (dataWithoutFrontmatter === "") {
-					new Notice("No content to split.");
-					return;
-				}
-
-				const splitLines = dataWithoutFrontmatter
-					.split(delimiter)
-					.map((line) => line.trim())
-					.filter((line) => line !== "");
-
-				if (splitLines.length === 0) {
-					new Notice("No content to split.");
-					return;
-				}
-
-				if (splitLines.length === 1) {
-					new Notice("Only one line found. Nothing to split.");
-					return;
-				}
-
-				const folderPath =
-					this.settings.saveFolderPath ||
-					file.parent?.path ||
-					this.settings.saveFolderPath;
-
-				try {
-					await this.app.vault.createFolder(folderPath);
-				} catch (err) {
-					//Folder already exists
-				}
-
-				let filesCreated = 0;
-				for (const [i, line] of splitLines.entries()) {
-					let fileName = line;
-					if (this.settings.useContentAsTitle) {
-						fileName = escapeInvalidFileNameChars(fileName);
-						fileName = trimForFileName(fileName, ".md");
-					} else {
-						fileName = `split-note-${Date.now() + i}`;
-					}
-
-					const filePath = normalizePath(`${folderPath}/${fileName}.md`);
-
-					try {
-						await this.app.vault.create(filePath, line);
-						filesCreated++;
-					} catch (err) {
-						if (err.message.includes("already exists")) {
-							const newFilePath = `${folderPath}/Split conflict ${crypto.randomUUID()}.md`;
-							try {
-								await this.app.vault.create(newFilePath, line);
-								filesCreated++;
-							} catch (err) {
-								console.error(err);
-								new Notice(`Error creating file: ${err.message}`);
-							}
-							continue;
-						}
-						new Notice(`Error creating file: ${err.message}`);
-						console.log(err);
-					}
-				}
-
-				if (filesCreated === splitLines.length && this.settings.deleteOriginalNote) {
-					await this.app.vault.delete(file);
-				}
-
-				new Notice(
-					"Split into " + filesCreated + " note" + (filesCreated > 1 ? "s" : "") + ".",
-				);
+				this.splitNoteByDelimiter(file);
 			},
 		});
 	}
 
-	onunload() {}
+	onunload() { }
+
+	private async splitNoteByDelimiter(file: TFile) {
+		//Obsidian will store `\n`` as `\\n` in the settings
+		const delimiter = this.settings.delimiter.replace(/\\n/g, "\n");
+
+		if (delimiter === "") {
+			new Notice("No delimiter set. Please set a delimiter in the settings.");
+			return;
+		}
+
+		const fileData = await this.app.vault.cachedRead(file);
+		const frontmatterEndIndex = findFrontmatterEndIndex(fileData);
+
+		let dataWithoutFrontmatter = fileData;
+		//Ignore frontmatter
+		if (frontmatterEndIndex !== -1) {
+			dataWithoutFrontmatter = dataWithoutFrontmatter.slice(frontmatterEndIndex + 1);
+		}
+		if (dataWithoutFrontmatter === "") {
+			new Notice("No content to split.");
+			return;
+		}
+
+		const splitLines = dataWithoutFrontmatter
+			.split(delimiter)
+			.map((line) => line.trim())
+			.filter((line) => line !== "");
+
+		if (splitLines.length === 0) {
+			new Notice("No content to split.");
+			return;
+		}
+
+		if (splitLines.length === 1) {
+			new Notice("Only one line found. Nothing to split.");
+			return;
+		}
+
+		const folderPath =
+			this.settings.saveFolderPath ||
+			file.parent?.path ||
+			this.settings.saveFolderPath;
+
+		try {
+			await this.app.vault.createFolder(folderPath);
+		} catch (err) {
+			//Folder already exists
+		}
+
+		let filesCreated = 0;
+		for (const [i, line] of splitLines.entries()) {
+			let fileName = line;
+			if (this.settings.useContentAsTitle) {
+				fileName = escapeInvalidFileNameChars(fileName);
+				fileName = trimForFileName(fileName, ".md");
+			} else {
+				fileName = `split-note-${Date.now() + i}`;
+			}
+
+			const filePath = normalizePath(`${folderPath}/${fileName}.md`);
+
+			try {
+				await this.app.vault.create(filePath, line);
+				filesCreated++;
+			} catch (err) {
+				if (err.message.includes("already exists")) {
+					const newFilePath = `${folderPath}/Split conflict ${crypto.randomUUID()}.md`;
+					try {
+						await this.app.vault.create(newFilePath, line);
+						filesCreated++;
+					} catch (err) {
+						console.error(err);
+						new Notice(`Error creating file: ${err.message}`);
+					}
+					continue;
+				}
+				new Notice(`Error creating file: ${err.message}`);
+				console.log(err);
+			}
+		}
+
+		if (filesCreated === splitLines.length && this.settings.deleteOriginalNote) {
+			await this.app.vault.delete(file);
+		}
+
+		new Notice(
+			"Split into " + filesCreated + " note" + (filesCreated > 1 ? "s" : "") + ".",
+		);
+	}
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
